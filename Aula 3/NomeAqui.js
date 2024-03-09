01fdb875c85fd58ed46861756f2c7752d9d08627
N4atan <162522588+N4atan@users.noreply.github.com>
2024-03-08 21:56:06 -0300

    Add files via upload

---
+++ Aula 3/NomeAqui.js
@@ -0,0 +1,11 @@
+//Variáveis, 'let' posso alterar mais tarde, 'const' não dá
+let nome = "Chat1"
+let versão = 0.1 
+
+
+
+//Imprimi algo no terminal(console)
+console.log("Olá Mundo,", "Sou o", nome, 'Minha atual versão é', versão, "." )
+
+//Para ver o tipo
+console.log(typeof nome)
---
+++ Aula 3/Prática.js
@@ -0,0 +1,10 @@
+//Variáveis
+const nome = "Natan Gabriel"
+const sobrenome = "Wildner da Silva"
+let idade = 17
+let souEstudante = true
+
+//Terminal
+console.log("Olá, me chamo", nome, sobrenome + ".", "Tenho", idade,"anos de idade.", "Sou estudante?", souEstudante + ".")
+
+
